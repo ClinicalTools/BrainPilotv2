f1b6cdcc9aedc9e07a08cde4250232dbf6311d3f
CTVilgalys <vilgalys@clinicaltools.com>
2018-09-25 17:45:49 -0400

    Updates!

---
+++ Assets/Scripts/Data/Interfaces/ISelectable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ISelectable
+{
+    /// <summary>
+    /// Register a listener to get updates from the selectable asset.  
+    /// </summary>
+    /// <param name="listener"></param>
+    void RegisterListener(SelectableListener listener);
+
+    /// <summary>
+    /// Unregister a listener from a selectable.
+    /// </summary>
+    /// <param name="listener"></param>
+    void UnregisterListener(SelectableListener listener);
+
+    void LoadState(SelectableState state);
+
+    void UnloadState(SelectableState state);
+
+    void ActivateState(SelectableState state, bool deactivateAllOthers = false);
+
+    void DeactivateState(SelectableState state);
+
+    List<SelectableState> GetActiveStates();
+
+}
