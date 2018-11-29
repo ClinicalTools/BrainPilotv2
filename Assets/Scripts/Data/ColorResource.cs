028fcf520ab3a765503926ca43f0b21bec4c5cbb
CTVilgalys <vilgalys@clinicaltools.com>
2018-11-29 17:26:12 -0500

    more UI work
    added ColorResource data asset,
    text mesh pro alpha fade

---
+++ Assets/Scripts/Data/ColorResource.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+[CreateAssetMenu]
+public class ColorResource : ScriptableObject
+{
+
+    public UnityEvent onValueChanged;
+
+    public Color Color {
+        get { return color; }
+        set {
+            color = value;
+            onValueChanged.Invoke();
+        }
+
+    }
+    [SerializeField]
+    protected Color color;
+
+}
---
+++ Assets/TweenTMPAlpha.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TweenTMPAlpha : MonoBehaviour {
+
+    [Range(0f, 1f)]
+    public float fadeOutAlpha;
+    [Range(0f, 1f)]
+    public float fadeInAlpha;
+
+    public float fadeTime = 0.3f;
+    public AnimationCurve curve = AnimationCurve.EaseInOut(0, 0, 1f, 1f);
+
+    public TextMeshPro textMesh;
+
+    [ContextMenu("Fade In")]
+    public void FadeIn()
+    {
+        StopAllCoroutines();
+        StartCoroutine(Fade(fadeOutAlpha, fadeInAlpha));
+    }
+
+    [ContextMenu("Fade Out")]
+    public void FadeOut()
+    {
+        StopAllCoroutines();
+        StartCoroutine(Fade(fadeInAlpha, fadeOutAlpha));
+    }
+
+    protected IEnumerator Fade(float start, float end)
+    {
+        float elapsedTime = 0;
+        textMesh = textMesh ?? GetComponent<TextMeshPro>();
+
+        while (elapsedTime <= fadeTime)
+        {
+            float t = elapsedTime / fadeTime;
+            float alpha = Mathf.LerpUnclamped(start, end, curve.Evaluate(t));
+            textMesh.alpha = alpha;
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+}
