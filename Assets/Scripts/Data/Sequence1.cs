480c9c587f33a10386212381a60ed89a0472809a
Ian <mckinnon@clinicaltools.com>
2019-03-05 19:37:14 -0500

    Beginning work on new structure
    Scenes will be layered

---
+++ Assets/Scripts/Data/Sequence1.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sequence1 : MonoBehaviour {
+	public string sequenceTitle;
+	public SequenceElement1[] steps;
+
+	[SerializeField]
+	private bool _active;
+	[SerializeField]
+	private int _stepIdx = 0;
+
+	public SequenceElement1 GetActiveStep()
+	{
+		return steps[_stepIdx];
+	}
+
+	public void AdvanceSequence()
+	{
+		Debug.Log("Advancing Sequence in sequence");
+		if (_stepIdx == steps.Length - 1) {
+			FinishSequence();
+		} else {
+			steps[_stepIdx].Deactivate();
+			_stepIdx++;
+			steps[_stepIdx].Activate();
+		}
+	}
+
+	public void RecedeSequence()
+	{
+		if (_stepIdx == 0) {
+			return;
+		} else {
+			steps[_stepIdx].Deactivate();
+			_stepIdx--;
+			steps[_stepIdx].Activate();
+		}
+	}
+
+	
+
+	public void StartSequence()
+	{
+		if (steps.Length == 0) {
+			_active = false;
+			return;
+		}
+		steps[_stepIdx].Activate();
+		_active = true;
+	}
+
+	public void PauseSequence()
+	{
+		steps[_stepIdx].Deactivate();
+		_active = false;
+	}
+
+	public void FinishSequence()
+	{
+		steps[_stepIdx].Deactivate();
+		_active = false;
+		ResetSequence();
+	}
+
+	public void ResetSequence()
+	{
+		_stepIdx = 0;
+	}
+
+	public bool IsActive()
+	{
+		return _active;
+	}
+}
