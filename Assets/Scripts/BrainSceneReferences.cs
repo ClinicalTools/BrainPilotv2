4c677fd07dfb900a8f9dad1c1a7008762c250294
Ian M <mckinnon@clinicaltools.com>
2019-03-10 23:46:33 -0400

    Way to group scenes in editor

---
+++ Assets/Scripts/BrainSceneReferences.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu]
+public class BrainSceneReferences : ScriptableObject {
+
+	public string title;
+	public string description;
+
+	public List<SceneField> sceneReferences = new List<SceneField>();
+
+	public SceneField GetScene(string sceneName)
+	{
+		return sceneReferences[0];
+	}
+
+	public SceneField GetScene(int buildIdx)
+	{
+		return sceneReferences[0];
+	}
+}
