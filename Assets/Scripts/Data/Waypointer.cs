12cfe8bd471892839de703d9d4bb894253ce2fa0
Benjamin Reinhart <the.reinhart@gmail.com>
2019-04-16 12:20:30 -0400

    Still getting waypoints under control

---
+++ Assets/Scripts/Data/Waypointer.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypointer : MonoBehaviour {
+	
+	[System.Serializable]
+	public class PlatformInformation
+	{
+		public Vector3 waypointLocation;
+		//public Transform waypointLocation;
+
+		public Transform lookAtPoint;
+		//public Vector3 lookAtDirection;
+
+		public float scaleVal;
+	}
+	
+	[SerializeField]
+	private PlatformInformation platformInformation;
+	
+	#if UNITY_EDITOR
+	[ContextMenu("Input Camera Position")]
+	public void InputCameraPos()
+	{
+		Transform cam = UnityEditor.SceneView.lastActiveSceneView.camera.transform;
+		if (cam == null) {
+			Debug.LogWarning("Scene camera not found!");
+			return;
+		}
+		platformInformation.waypointLocation = cam.position;
+	}
+
+	[ContextMenu("Clear Platform Info")]
+	public void ClearPlatformInfo()
+	{
+		platformInformation.lookAtPoint = null;
+		platformInformation.scaleVal = 0;
+		platformInformation.waypointLocation = Vector3.zero;
+	}
+	#endif
+	public void Activate()
+	{
+		HandlePlatformMovement();
+	}	
+
+	public void HandlePlatformMovement()
+	{
+		StartCoroutine(IterateMovement());
+	}
+
+		private IEnumerator IterateMovement()
+	{
+		float time = 0;
+		float totalTime = 3f;
+		while (time < totalTime) {
+			/*LerpMovement();
+			LerpRotation();
+			LerpScale(); */
+			time += Time.deltaTime;
+			yield return null;
+		}
+	}
+
+	public PlatformInformation GetPlatformInfo()
+	{
+		return platformInformation;
+	}
+}
