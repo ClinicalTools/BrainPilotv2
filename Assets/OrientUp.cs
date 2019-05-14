2db0d610c3fa6b5f6b686dd640eeeac1d3884593
Ian <mckinnon@clinicaltools.com>
2019-05-14 15:22:13 -0400

    Quick select menu work

---
+++ Assets/OrientUp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrientUp : MonoBehaviour {
+
+	RectTransform rt;
+	Vector3 eulers;
+	private void Awake()
+	{
+		rt = GetComponent<RectTransform>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (rt.eulerAngles.z != 0) {
+			eulers = rt.eulerAngles;
+			eulers.z = 0;
+			rt.eulerAngles = eulers;
+		}
+	}
+}
