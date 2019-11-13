dbadd21f7d58b7d9271419ab0c86a7f69098286a
gallagherClinical <gallagher@clinicaltools.com>
2019-11-13 15:07:04 -0500

    particle start

---
+++ Assets/ParticleFollowPath_Test01.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleFollowPath_Test01 : MonoBehaviour
+{
+	public string pathName;
+	public float time;
+
+    void Start()
+    {
+		iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath(pathName), "easetype", iTween.EaseType.easeInOutSine, "time", time) );
+        
+    }
+
+}
